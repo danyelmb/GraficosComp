f0f142580fba7068fda76c28d8aebf5669380d79
Jesús <was121995@gmail.com>
2018-05-19 20:58:16 +0200

    clear

--- Thanos/main.cpp
+++ Thanos/main.cpp
@@ -6,7 +6,7 @@
 
 void ejes();
 void initFunc();
-
+void universe();
 void destroyFunc();
 void funReshape(int w, int h);
 void funDisplay();
@@ -61,90 +61,12 @@ GLfloat PL1[] = {1.5f, 2.0f,0.0f, 1.0f };
 #define NT 1
 GLuint  objTexture[NT];
 GLfloat old_y=2,old_x=-3;
-
-
-
-void time(){
+void universe(){
     
-    GLfloat Kq[] = { 0.2f, 0.2f, 0.2f, 1.0f };
-    GLfloat K2[] = { 0.0f, 0.3f, 0.0f, 1.0f };
-    GLfloat Kz[] = { 0.5f, 0.5f, 0.5f, 1.0f };
-    glMaterialfv(GL_FRONT, GL_AMBIENT  , Kq);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE  , K2);
-    glMaterialfv(GL_FRONT, GL_SPECULAR , Kz);
-    glMaterialf (GL_FRONT, GL_SHININESS, 100.0f);
-     glutSolidSphere(0.5,50,50);
-    }
-
-void reality(){
-  
-    GLfloat Kp[] = { 0.2f, 0.2f, 0.2f, 1.0f };
-    GLfloat Kl[] = { 0.5f, 0.0f, 0.0f, 1.0f };
-    GLfloat Km[] = { 0.5f, 0.5f, 0.5f, 1.0f };
-    glMaterialfv(GL_FRONT, GL_AMBIENT  , Kp);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE  , Kl);
-    glMaterialfv(GL_FRONT, GL_SPECULAR , Km);
-    glMaterialf (GL_FRONT, GL_SHININESS, 100.0f);
-              glutSolidSphere(0.5,50,50);  
 }
-
-void space(){
+void gaunlet(){
     
-    GLfloat Ku[] = { 0.2f, 0.2f, 0.2f, 1.0f };
-    GLfloat Kj[] = { 0.3f, 0.3f, 0.6f, 1.0f };
-    GLfloat Kn[] = { 0.5f, 0.5f, 0.5f, 1.0f };
-    glMaterialfv(GL_FRONT, GL_AMBIENT  , Ku);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE  , Kj);
-    glMaterialfv(GL_FRONT, GL_SPECULAR , Kn);
-    glMaterialf (GL_FRONT, GL_SHININESS, 100.0f);
-              glutSolidSphere(0.5,50,50);
 }
-
-void soul(){
-    
-    GLfloat Kw[] = { 0.2f, 0.2f, 0.2f, 1.0f };
-    GLfloat K1[] = { 0.5f, 0.2f, 0.01f, 1.0f };
-    GLfloat Kx[] = { 0.5f, 0.5f, 0.5f, 1.0f };
-    glMaterialfv(GL_FRONT, GL_AMBIENT  , Kw);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE  , K1);
-    glMaterialfv(GL_FRONT, GL_SPECULAR , Kx);
-    glMaterialf (GL_FRONT, GL_SHININESS, 100.0f);
-              glutSolidSphere(0.5,50,50);
-}
-
-void power(){
-    
-    GLfloat Kc[] = { 0.2f, 0.2f, 0.2f, 1.0f };
-    GLfloat Kf[] = { 0.5f, 0.0f, 0.5f, 1.0f };
-    GLfloat Kv[] = { 0.5f, 0.5f, 0.5f, 1.0f };
-    glMaterialfv(GL_FRONT, GL_AMBIENT  , Kc);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE  , Kf);
-    glMaterialfv(GL_FRONT, GL_SPECULAR , Kv);
-    glMaterialf (GL_FRONT, GL_SHININESS, 100.0f);
-               glutSolidSphere(0.5,50,50);
-}
-
-void ejes(){
-    glEnable (GL_LINE_STIPPLE);
-    glLineStipple(1,255);
-    glLineWidth(2.0f);
-    glColor3f(0.5,0.5,0.5);
-    glBegin(GL_LINES);
-    glVertex3f(-10,0,0);
-    glVertex3f(10,0,0);
-   
-    glVertex3f(0,-10,0);
-    glVertex3f(0,10,0);
-    
-    glVertex3f(0,0,-10);
-    glVertex3f(0,0,10);
-    glEnd();
-    
-glDisable (GL_LINE_STIPPLE);
-
-    glLineWidth(1.0f);
-}
-
 
 int main(int argc, char** argv) {
 
@@ -329,122 +251,11 @@ void funDisplay() {
                center[0], center[1], center[2],
                    up[0],     up[1],     up[2]);
 
-            glColor3f(0.0f, 0.0f, 0.0f);
-            glPushMatrix();
-            
-            glRotatef(Lp,0,1,0);
-            glTranslatef( 1.5f, 2.0f,0.0f );
-           
-    // Definimos el material del objeto
-    GLfloat Ke[] = { luz_r,luz_v,luz_b,1};
-    GLfloat Kr[] = {luz_r,luz_v,luz_b,1};
-    GLfloat Kt[] = { luz_r,luz_v,luz_b,1};
-    glMaterialfv(GL_FRONT, GL_AMBIENT  , Ke);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE  , Kr);
-    glMaterialfv(GL_FRONT, GL_SPECULAR , Kt);
-    glMaterialf (GL_FRONT, GL_SHININESS, 100.0f);
-            glutSolidSphere(0.05,50,50);
-            glPopMatrix();
-    ejes();
-    drawLights();
-    /*
-    glPushMatrix();
-    glTranslatef(0,0,2);
-    glColor3f(0.6f,0.6f,0.3f);
-    glutSolidSphere(0.25,50,50);
-    glScalef(1.0,1.0,0.25);
-    glPopMatrix();
-    */
-    
-    glPushMatrix();
-    
-    glTranslatef(-1,0,0);
-    glRotatef(rotY_base,1,1,1);
-    glTranslatef(1,0,0);
-    
-    glScalef(0.25,0.25,0.25);
-    drawCube();
-    glPopMatrix();
-    glPushMatrix();
-    
-    glRotatef(rotY_base,-1,-1,-1);
-    glTranslatef(1,0,0);
-    
-    glScalef(0.25,0.25,0.25);
-    drawCube();
-    glPopMatrix();
-     glPushMatrix();
-    
-    glRotatef(rotY_base,-1,-1,-1);
-    glTranslatef(1,0,0);
-    
-    glScalef(0.25,0.25,0.25);
-    drawCube();
-    glPopMatrix();
+   //Dibujamos las luces
+ 
+
    // Intercambiamos los buffers
     glutSwapBuffers();
-    
-}
-void drawCube() {
-    
- // Definimos el material del cubo
-    GLfloat Ka[] = { 0.2f, 0.2f, 0.2f, 1.0f };
-    GLfloat Kd[] = { 1,1,1, 1.0f };
-    GLfloat Ks[] = { 0.5f, 0.5f, 0.5f, 1.0f };
-    glMaterialfv(GL_FRONT, GL_AMBIENT  , Ka);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE  , Kd);
-    glMaterialfv(GL_FRONT, GL_SPECULAR , Ks);
-    glMaterialf (GL_FRONT, GL_SHININESS, 100.0f);
-        
- // Definimos el cubo
-     glEnable(GL_TEXTURE_2D);
-     glBindTexture(GL_TEXTURE_2D, objTexture[0]);
-    glBegin(GL_QUADS);
-     // CARA DERECHA (x = 1)
-        glNormal3f( 1.0f,  0.0f,  0.0f);
-        glTexCoord2f(0.0f, 0.0f);
-        glVertex3f( 1.0f, -1.0f,  1.0f); 
-        glTexCoord2f(1.0f, 0.0f);
-        glVertex3f( 1.0f, -1.0f, -1.0f); 
-        
-        glTexCoord2f(1.0f, 1.0f);
-        glVertex3f( 1.0f,  1.0f, -1.0f); 
-        glTexCoord2f(0.0f, 1.0f);
-        glVertex3f( 1.0f,  1.0f,  1.0f);   
-        glEnd();
-        glDisable(GL_TEXTURE_2D);
-        glBegin(GL_QUADS);
-     // CARA IZQUIERDA (x = -1)
-        glNormal3f(-1.0f,  0.0f,  0.0f);
-        glVertex3f(-1.0f, -1.0f,  1.0f);
-        glVertex3f(-1.0f,  1.0f,  1.0f);
-        glVertex3f(-1.0f,  1.0f, -1.0f);
-        glVertex3f(-1.0f, -1.0f, -1.0f);
-     // CARA SUPERIOR (y = 1)
-        glNormal3f( 0.0f,  1.0f,  0.0f);
-        glVertex3f( 1.0f,  1.0f,  1.0f);
-        glVertex3f( 1.0f,  1.0f, -1.0f);
-        glVertex3f(-1.0f,  1.0f, -1.0f); 
-        glVertex3f(-1.0f,  1.0f,  1.0f);
-     // CARA INFERIOR (y = -1)
-        glNormal3f( 0.0f, -1.0f,  0.0f);
-        glVertex3f( 1.0f, -1.0f,  1.0f);    
-        glVertex3f(-1.0f, -1.0f,  1.0f);
-        glVertex3f(-1.0f, -1.0f, -1.0f);    
-        glVertex3f( 1.0f, -1.0f, -1.0f);
-     // CARA DELANTERA (z = 1)
-        glNormal3f( 0.0f,  0.0f,  1.0f);
-        glVertex3f(-1.0f, -1.0f,  1.0f);
-        glVertex3f( 1.0f, -1.0f,  1.0f);
-        glVertex3f( 1.0f,  1.0f,  1.0f);
-        glVertex3f(-1.0f,  1.0f,  1.0f);
-     // CARA TRASERA (z = -1)
-        glNormal3f( 0.0f,  0.0f, -1.0f);
-        glVertex3f(-1.0f, -1.0f, -1.0f);
-        glVertex3f(-1.0f,  1.0f, -1.0f);
-        glVertex3f( 1.0f,  1.0f, -1.0f);  
-        glVertex3f( 1.0f, -1.0f, -1.0f);
-    glEnd();
     
 }
 void funKeyboard(int key, int x, int y) {
@@ -452,40 +263,20 @@ void funKeyboard(int key, int x, int y) {
     switch(key) {
         case GLUT_KEY_UP:
            
-             luz_r+= 0.1;
-             glDisable(GL_LIGHT1);
-              glEnable(GL_LIGHT2);
-             if(luz_r<=+1){
-                luz_r=+1;
-            }
+          
             break;
         case GLUT_KEY_DOWN:
             
            
-             glDisable(GL_LIGHT2);
-              glEnable(GL_LIGHT3);
-               luz_r -= 5.0f;
-             if(luz_r>=0){
-                luz_r=0;
-            }
+          
             break;
         case GLUT_KEY_RIGHT:
             
-             glDisable(GL_LIGHT3);
-              glEnable(GL_LIGHT4);
-            rotY_green += 5.0f;
-             if(rotY_green>=+130){
-                rotY_green=+130;
-            }
+            
             break;
         case GLUT_KEY_LEFT:
             
-             glDisable(GL_LIGHT4);
-              glEnable(GL_LIGHT5);
-             rotY_green -= 5.0f;
-             if(rotY_green<=-130){
-                rotY_green=-130;
-            }
+            
            
             break;
       
@@ -575,34 +366,7 @@ void turnoff(unsigned char Key){
 }
 void MyKeyboardFunc(unsigned char Key, int x, int y){
     
-    switch(Key) {
-        
-        
-        case 'p':
-           Lp -= 3.5;
-        break;
-        case 'l': if(luz_a){
-            luz_a=0;
-              glDisable(GL_LIGHT0);
-        }else{ luz_a=1;
-          glEnable(GL_LIGHT0);}
-        break;
-        case 'r': rotY_base += 5; break;
-        case 'R': rotY_base -= 5; break; break;
-        case 'S': rotY_base -= 5; break; break;
-        case 's': rotY_base -= 5; break; break;
-        
-        case 'M': mind=1;
-        turnoff(light);
-        light='1';
-        glEnable(GL_LIGHT1);
-        break;
-        case 'd': desZ-=0.01;if(desZ<=-0.5){
-            desZ=-0.5;}break;
-        case 'D': desZ+=0.01;
-        if(desZ>=0){ 
-            desZ=0;}break;
-    }
+    
     glutPostRedisplay();
     
 }
